0fa4704e7bcfc2382e6251948d787eaa1921bc16
AlboLock <103566140+AlboLock@users.noreply.github.com>
2025-01-30 16:06:11 +0100

    Pequeñas corecion
    Añadido llamada a funciones. Correción funciones de añadir y eliminar (las funciones aceptan como parametro solo el nombre, el vector lo declaramos al principio y usamos solo ese)

--- 29-CRUDdeAlumnosConVector/main.js
+++ 29-CRUDdeAlumnosConVector/main.js
@@ -1,36 +1,31 @@
 // Crear función que añade un nombre a un vector
+let arrayNombres = [];
 
-function anadirNombre(array, nombre) {
-
-    if (!Array.isArray(array)) {                                         // Verificar que el primer parámetro es realmente un array
-        console.log("Error: El primer parámetro debe ser un array.");
-        return;  
-    }
-    
+function anadirNombre(nombre) {
     if (typeof nombre !== "string") {                                   // Verificar que el nombre es una cadena de texto
         console.log("Error: El segundo parámetro debe ser una cadena de texto.");
         return; 
     }
 
-    array.push(nombre);               //Añadir elemento a array
-    return array;
+    arrayNombres.push(nombre);               //Añadir elemento a array
+    console.log(arrayNombres);
 }
 
 
 
 // Crear función que elimina un nombre de un vector
 
-function eliminarNombre(array, nombre) {
+function eliminarNombre(nombre) {
 
-    const indice = array.indexOf(nombre);
+    const indice = arrayNombres.indexOf(nombre);
 
     if (indice !== -1) {
-        array.splice(indice, 1);                    // Si el nombre existe en el array, eliminarlo
+        arrayNombres.splice(indice, 1);                    // Si el nombre existe en el array, eliminarlo
     } else {
         console.log("Error: El nombre no se encontró en el array.");
     }
 
-    return array;
+    console.log(arrayNombres);
 }
 
 
